fa8c28283284867e110b2f4157ae7e447a8bce99
Are Bqllon <ari331962@gmail.com>
2026-06-30 13:04:22 -0500

    Se agrego cambios #2 al modulo de servicios

--- router/enrutamiento.router.js
+++ router/enrutamiento.router.js
@@ -17,13 +17,14 @@ router.delete('/clientes/:id', clienteController.eliminar);
 router.get('/formularioP', productoController.formularioP)
 router.get('/productos', productoController.consultar);
 router.get('/productos/:id', productoController.consultarId);
-router.post('/productos', productoController.crear);
+router.post('/productos', productoController.registrar);
 router.put('/productos/:id', productoController.actualizar);
 router.delete('/productos/:id', productoController.eliminar);
 
+router.get('/formularioP', servicioController.formularioS)
 router.get('/servicios', servicioController.consultar);
 router.get('/servicios/:id', servicioController.consultarId);
-router.post('/servicios', servicioController.crear);
+router.post('/servicios', servicioController.registrar);
 router.put('/servicios/:id', servicioController.actualizar);
 router.delete('/servicios/:id', servicioController.eliminar);
 
